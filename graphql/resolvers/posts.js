4be9edaf89d8bd4529dba0e5392bd1f4382bf92b
Kevin Molina <kevmo1987@gmail.com>
2020-02-09 18:27:52 -0800

    -- like/unlike mutation -- create comment -- delete comment -- new post subscription implementation added.

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
@@ -39,6 +39,11 @@ module.exports = {
       });
 
       const post = await newPost.save();
+
+      context.pubsub.publish("NEW_POST", {
+        newPost: post
+      });
+
       return post;
     },
     async deletePost(_, { postId }, context) {
@@ -55,6 +60,30 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find(like => like.username === username)) {
+          // post has already been liked...unlike the post
+          post.likes = post.likes.filter(like => like.username !== username);
+        } else {
+          // Post has not been liked...like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    }
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST")
     }
   }
 };
